c911cf935c7cc7ce581d1a53e0310cd91306025d
Lanaetan <peiyantan03@gmail.com>
2025-03-28 23:25:27 +0800

    Chats Dialog Screen

--- screens/ChatListScreen.tsx
+++ screens/ChatListScreen.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {StatusBar, StyleSheet, Text,View} from "react-native";
 import ChatListItem from '../assets/components/ChatListItem'
+import ChatsScreen from './ChatsScreen';
 import ChatScreen from './ChatScreen';
 
 const App = () => {
--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,20 +1,34 @@
 import React from "react";
-import {Text,View, FlatList} from "react-native";
-import chats from '../assets/data/chats.json';
-// import { FlatList } from "react-native-gesture-handler";
-import ChatListItem from "../assets/components/ChatListItem";
+import { Text, View, ImageBackground, StyleSheet, FlatList } from "react-native";
+import Message from "../assets/components/Message";
+
+// import bg from '../assets/images/luguang.jpg';
+import messages from '../assets/data/messages.json'
 
 const ChatScreen = () => {
+
+  const bg = require('../assets/images/luguang.jpg');
     return(
-      <View>
-        <FlatList 
-          data={chats}
-          renderItem={({ item }) => 
-            <ChatListItem chat={item} />
-          }
+      // <ImageBackground source={bg} style={styles.bg}></ImageBackground>
+      <View style={styles.bg}>
+      
+        <FlatList
+          data={messages}
+          renderItem={({item}) => <Message message={item} />}
+          style={styles.list}
+          inverted
         />
+
+        
       </View>
     )
 }
+
+const styles = StyleSheet.create({
+  bg: {
+    flex: 1,
+    backgroundColor: '#e3e6e5',
+  }
+})
 
 export default ChatScreen;
